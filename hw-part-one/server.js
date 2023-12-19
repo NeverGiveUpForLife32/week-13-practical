7c71c6504c42d8abf7935c03be296268ffee1459
NeverGiveUpForLife32 <clflem687@yahoo.com>
2023-12-18 22:55:39 -0500

    Magic 8 Ball express application created.

--- hw-part-one/server.js
+++ hw-part-one/server.js
@@ -15,6 +15,44 @@ app.get("/tip/:total/:tipPercentage", (req, res) => {
   res.send(`${total * tipPercentage}`);
 });
 
-app.listen(3001, () => {
-  console.log("Nice!");
+app.get("/magic/:question/", (req, res) => {
+  let questions = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes definitely",
+    "You may rely on it",
+    "As I see it yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+    "Reply hazy try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful",
+  ];
+  questions = questions[Math.floor(Math.random() * questions.length)];
+  // randomAnswer = req.params.question;
+  res.send(`Will I Be A Millionaire? ${questions}`);
+});
+
+//add show route
+//   app.get('/answers/:randomAnswer')
+
+//   res.send(millionaire);
+// });
+
+// app.listen(3001, () => {
+//   console.log("Nice!");
+// });
+
+app.listen(3002, () => {
+  console.log("Get it!!!");
 });
