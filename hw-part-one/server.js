3123f7da5db2b903d37c4242cb7fdba6d1a00bca
NeverGiveUpForLife32 <clflem687@yahoo.com>
2023-12-18 20:47:10 -0500

    Tip Calculator express application created.

--- hw-part-one/server.js
+++ hw-part-one/server.js
@@ -9,6 +9,12 @@ app.get("/:name", (req, res) => {
   res.send(`greeting ${req.params.name}`);
 });
 
+app.get("/tip/:total/:tipPercentage", (req, res) => {
+  const total = parseInt(req.params.total);
+  const tipPercentage = parseInt(req.params.tipPercentage) / 100;
+  res.send(`${total * tipPercentage}`);
+});
+
 app.listen(3001, () => {
   console.log("Nice!");
 });
